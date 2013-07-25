e2c3e0e7bf1be370f42a73984073e4e7c6ca44b2
dzucconi <mail@damonzucconi.com>
2013-07-25 17:07:48 -0400

    More specs

--- spec/immanence_spec.rb
+++ spec/immanence_spec.rb
@@ -4,7 +4,12 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe "Immanence" do
   before do
-    @Δ = Immanence::Control::LEVENSHTEIN
+    class Application < Immanence::Control
+      route :get, "/resource/:id" do; end
+    end
+
+    @immanence  = Immanence::Control
+    @Δ          = @immanence::LEVENSHTEIN
   end
   it "should calculate the Levenshtein difference between two strings (0)" do
     @Δ["kitten", "sitten"].should eq 1
@@ -16,5 +21,29 @@ describe "Immanence" do
 
   it "should calculate the Levenshtein difference between two strings (2)" do
     @Δ["sittin", "sitting"].should eq 1
+  end
+
+  it "should conjugate a verb and path into a method name" do
+    @immanence.send(:conjugate, :get, "/resource/:id").should eq "immanent_get_/resource/:id"
+  end
+
+  it "should extract parameters out of the request path" do
+    @immanence.send(:ascertain, "immanent_get_/resource/:id", "/resource/1000").should eq({ id: "1000" })
+  end
+
+  it "should be able to extract multiple parameters out of the request path" do
+    @immanence.send(:ascertain, "immanent_get_/resource/:resource_id/child/:id", "/resource/1000/child/10").should eq({ resource_id: "1000", id: "10" })
+  end
+
+  it "should provide a DSL for defining routes" do
+    Application.methods.include?(:"immanent_get_/resource/:id").should be_true
+  end
+
+  it "should have a default response" do
+    Application.>>.should eq([200, {"Content-Type"=>"text/json", "Content-Length"=>"4"}, ["\"ok\""]])
+  end
+
+  it "should be able to respond with a JSON object" do
+    Application.>>({ id: "1000" }).should eq([200, {"Content-Type"=>"text/json", "Content-Length"=>"13"}, ["{\"id\":\"1000\"}"]])
   end
 end
